06a72f1a127139c674231b4060103f9c4224ae69
TimTernar <somthing900@gmail.com>
2026-01-26 12:57:08 +0100

    implemented first lambda expression

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ void main() {
 
     Location stozice = new Location(1, "Ljubljana", "Vojkova street", 500);
     Location UM = new Location(2, "Maribor", "Koroška street", 100);
+    Location Pula = new Location(3, "Pula", "Cesta cesta", 160);
 
     stozice.addSeat(new Seat(1, 1, 1));
     stozice.addSeat(new Seat(2, 1, 2));
@@ -15,6 +16,8 @@ void main() {
     //User u = new User(1, "Tim", "Ternar", 22);
 
     Concert concert = new Concert(1, "Rock concert in Stožice", LocalTime.now(), stozice, 18.50, "Parni Valjak", "Rock", true, 120);
+    Concert concert2 = new Concert(2, "Rock concert in Arena Pula", LocalTime.now(), Pula, 18.50, "Bajaga i Instruktori", "Rock", false, 180);
+
     Conference conference = new Conference(1, "Conference about Java and memory optimizazion", LocalTime.now(), stozice, 5.50, "Do better with Java", "Walter White", true);
     Conference conference2 = new Conference(3, "Why Manchaster United is finally back", LocalTime.now(), stozice, 4.50, "Michael Carrick is the saviour", "Tim Ternar", true);
 
@@ -53,4 +56,14 @@ void main() {
     // 4) By name
     List<Conference> byName = Conference.getByName(conferences, "Michael Carrick is the saviour");
     byName.forEach(System.out::println);
+
+    ArrayList<Concert> concerts = new ArrayList<>();
+    concerts.add(concert);
+    concerts.add(concert2);
+
+    //concert methods
+    //1) return all concert genres
+    List<Concert> returnGenres = Concert.getByGnere(concerts, "Rock");
+    returnGenres.forEach(System.out::println);
+
 }
